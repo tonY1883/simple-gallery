b0f5a1150102b15b96d37e9ccfbac6899f46c365
wyrd <vol1883@gmail.com>
2023-08-26 12:39:40 +0800

    Add thumnail creation

--- index.py
+++ index.py
@@ -7,10 +7,19 @@ import json
 from tqdm import tqdm
 import urllib.parse
 import subprocess
+import multiprocessing
 
 dir_name = sys.argv[1]
 data_dir_path = Path('.simple_gallery_data')
+data_dir_path.mkdir(parents=True, exist_ok=True)
+data_dir_path.joinpath('thumbnails').mkdir(exist_ok=True, parents=True)
 index = []
+
+
+def create_thumbnail(path_to_original, path_to_thumbnail):
+	#print(str(path_to_original) + " -> " + str(path_to_thumbnail))
+	#print(" ".join(['ffmpeg', '-nostdin', '-i', str(path_to_original), "-vf", "scale=trunc(oh*a/2)*2:min(500\,iw)", '-y', str(path_to_thumbnail)]))
+	subprocess.run(['ffmpeg', '-nostdin', '-i', str(path_to_original), "-vf", "scale=trunc(oh*a/2)*2:min(500\,iw)", '-y', str(path_to_thumbnail)], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT).stdout.decode('utf-8')
 
 #Only select formats currently supported by browsers
 accept_file_formats = [
@@ -19,6 +28,7 @@ accept_file_formats = [
 files = list(itertools.chain.from_iterable(map((lambda x: list(Path(dir_name).rglob(x))), accept_file_formats)))
 total_count = len(files)
 pbar = tqdm(total = total_count, leave = True)
+p = multiprocessing.Pool(processes = max(1, int(multiprocessing.cpu_count()/2)))
 for idx, file in enumerate(files, start = 1):
 	try:
 		pbar.update(1)
@@ -27,11 +37,17 @@ for idx, file in enumerate(files, start = 1):
 			info = json.loads(info)
 			data = {}
 			data["name"] = file.name
+			data["index"] = idx
 			data["url"] = urllib.parse.quote(str(file))
+			thumbnail_path = data_dir_path.joinpath('thumbnails').joinpath(str(idx)).with_suffix('.jpg')
+			data["thumbnailUrl"] = urllib.parse.quote(str(thumbnail_path))
 			data["meta"] = info[0]
 			index.append(data)
+			#create_thumbnail(file, thumbnail_path)
+			p.apply_async(create_thumbnail,[file, thumbnail_path])
 	except:
 		print("Fail to read " + str(file))
-data_dir_path.mkdir(parents=True, exist_ok=True)
+p.close()
+p.join()
 with open(data_dir_path.joinpath('index.json'), 'w', encoding = 'utf8') as fp:
 	json.dump(index, fp, ensure_ascii = False)
