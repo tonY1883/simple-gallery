95a1738d7957acb72912fcad7c85629c4a436bf5
Tony Yip <tony.cnyip@gmail.com>
2023-08-26 18:51:04 +0800

    added large view for each image

--- gallery.js
+++ gallery.js
@@ -19,12 +19,26 @@ class SimpleGallery {
         this.gallery.innerHTML = '';
         let newContent = '';
         this.galleryImages.forEach((img) => {
-            newContent += `<div class="gallery-image-panel"><img src="${img.thumbnailUrl}" loading="lazy" alt="${img.name}"></div>`;
+            newContent += `<div class="gallery-image-panel"><img src="${img.thumbnailUrl}" loading="lazy" alt="${img.name}" onclick="galleryApp.displayImage(${img.index})"></div>`;
         });
         this.gallery.innerHTML = newContent;
     }
+    displayImage(index) {
+        const image = this.galleryImages.find(i => i.index === index);
+        console.info("loaded image", image);
+        this.image.src = image.url;
+        this.image.alt = image.name;
+        this.image.onload = () => { this.imageOverlay.classList.toggle('hidden'); };
+    }
+    hideImage() {
+        this.imageOverlay.classList.toggle('hidden');
+    }
     initialize() {
         this.gallery = document.querySelector('#gallery-wrapper');
+        this.imageOverlay = document.querySelector('#image-wrapper');
+        this.imageOverlayCloseBtn = document.querySelector('#close-btn');
+        this.imageOverlayCloseBtn.addEventListener('click', () => { this.hideImage(); });
+        this.image = document.querySelector('#the-image');
         this.loadImages(() => { this.displayImages(); });
     }
 }
