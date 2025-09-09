720c9c8781be436cc8c58aa7e7fef162d3e9b258
Tony Yip <tony.cnyip@gmail.com>
2025-09-09 14:10:29 +0800

    update js version

--- gallery.js
+++ gallery.js
@@ -1,16 +1,23 @@
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
-    if (kind === "m") throw new TypeError("Private method is not writable");
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
-    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
-var _DBHelper_dbName, _DBHelper_dbVersion, _DBHelper_dbInstance;
 class SimpleGallery {
+    #albums;
+    #galleryImages;
+    #currentImage;
+    #dbHelper;
+    #albumListDisplay;
+    #gallery;
+    #imageOverlay;
+    #imageOverlayCloseBtn;
+    #image;
+    #imageInfoButton;
+    #imageNextButton;
+    #imagePreviousButton;
+    #imageInfoPanel;
+    #imageInfoName;
+    #imageInfoTable;
+    #imageLocationMap;
+    //Lealet map library
+    leafletLib;
+    #leafletMap;
     static tableFromObject(data, title) {
         let dataTable = document.createElement("table");
         let body = dataTable.createTBody();
@@ -28,7 +35,7 @@ class SimpleGallery {
         this.initialize();
     }
     loadImages(callBack) {
-        this.dbHelper
+        this.#dbHelper
             .open()
             .then(() => fetch("hash.txt", { cache: "no-store" }))
             .then((response) => response.text())
@@ -39,27 +46,27 @@ class SimpleGallery {
             localStorage.setItem(HASH_STORAGE_KEY, hash);
             if (currentChecksum !== hash) {
                 console.info("Index outdated or missing, downloading new index");
-                return this.dbHelper
+                return this.#dbHelper
                     .clearIndex()
                     .then(() => fetch(".simple_gallery_data/index.json", { cache: "no-store" }))
                     .then((response) => response.json())
                     .then(async (images) => {
-                    await this.dbHelper.updateImageIndex(images);
+                    await this.#dbHelper.updateImageIndex(images);
                 });
             }
             else {
                 return Promise.resolve();
             }
         })
-            .then(() => this.dbHelper.getAllImages())
+            .then(() => this.#dbHelper.getAllImages())
             .then((data) => {
-            this.galleryImages = data;
-            this.albums = new Map();
+            this.#galleryImages = data;
+            this.#albums = new Map();
             data.forEach((element) => {
-                if (!this.albums.has(element.textMeta.Directory)) {
-                    this.albums.set(element.textMeta.Directory, new Array());
+                if (!this.#albums.has(element.textMeta.Directory)) {
+                    this.#albums.set(element.textMeta.Directory, new Array());
                 }
-                this.albums.get(element.textMeta.Directory).push(element);
+                this.#albums.get(element.textMeta.Directory).push(element);
             });
         })
             .then(() => callBack(this))
@@ -69,131 +76,131 @@ class SimpleGallery {
         });
     }
     displayAlbums() {
-        this.albumListDisplay.innerHTML = "";
+        this.#albumListDisplay.innerHTML = "";
         let newContent = "";
-        this.albums.forEach((imgs, dir) => {
+        this.#albums.forEach((imgs, dir) => {
             newContent += `<li onclick="galleryApp.displayImages('${dir}')">${dir}<li>`;
         });
-        this.albumListDisplay.innerHTML = newContent;
+        this.#albumListDisplay.innerHTML = newContent;
     }
     displayImages(album) {
-        this.gallery.innerHTML = "";
+        this.#gallery.innerHTML = "";
         let newContent = "";
-        this.albums.get(album).forEach((img) => {
+        this.#albums.get(album).forEach((img) => {
             newContent += `<div class="gallery-image-panel"><img src="${img.thumbnailUrl}" loading="lazy" alt="${img.name}" onclick="galleryApp.displayImage(${img.id})"></div>`;
         });
-        this.gallery.innerHTML = newContent;
+        this.#gallery.innerHTML = newContent;
     }
     displayImage(id) {
-        if (id === this.currentImage?.id) {
+        if (id === this.#currentImage?.id) {
             //simply unhide the detail view
             console.debug("Same image as current image, nothing to do");
-            this.imageOverlay.classList.remove("gone");
+            this.#imageOverlay.classList.remove("gone");
             return;
         }
-        let image = this.galleryImages.find((i) => i.id === id);
+        let image = this.#galleryImages.find((i) => i.id === id);
         if (!!image) {
-            this.currentImage = image;
+            this.#currentImage = image;
         }
         else {
             console.error(`Cannot find image with id ${id}`);
             return;
         }
-        console.info("loaded image", this.currentImage);
-        this.image.classList.add("hidden");
-        this.image.src = this.currentImage.url;
-        this.image.alt = this.currentImage.name;
-        this.imageInfoName.innerText = this.currentImage.name;
-        this.imageInfoTable.innerHTML = "";
+        console.info("loaded image", this.#currentImage);
+        this.#image.classList.add("hidden");
+        this.#image.src = this.#currentImage.url;
+        this.#image.alt = this.#currentImage.name;
+        this.#imageInfoName.innerText = this.#currentImage.name;
+        this.#imageInfoTable.innerHTML = "";
         const meta = {
             "File Meta": {
-                Format: this.currentImage.textMeta.FileType.trim(),
-                Size: this.currentImage.textMeta.FileSize.trim(),
-                Directory: this.currentImage.textMeta.Directory.trim(),
+                Format: this.#currentImage.textMeta.FileType.trim(),
+                Size: this.#currentImage.textMeta.FileSize.trim(),
+                Directory: this.#currentImage.textMeta.Directory.trim(),
             },
             Photography: {
-                Dimension: `${this.currentImage.textMeta.ImageSize.trim()} (${this.currentImage.textMeta.Megapixels} Megapixels)`,
-                Aperture: this.currentImage.textMeta.Aperture ? `f/${this.currentImage.textMeta.Aperture}` : null,
-                "Shutter Speed": this.currentImage.textMeta.ShutterSpeed?.trim(),
-                "Focal Length": this.currentImage.textMeta.FocalLength?.trim(),
-                ISO: this.currentImage.textMeta.ISO,
-                "White Balance": this.currentImage.textMeta.WhiteBalance?.trim(),
-                "Camera Model": !!this.currentImage.textMeta.Make
-                    ? !!this.currentImage.textMeta.Model
-                        ? this.currentImage.textMeta.Make.trim() + " " + this.currentImage.textMeta.Model.trim()
-                        : this.currentImage.textMeta.Make.trim()
-                    : !!this.currentImage.textMeta.Model
-                        ? this.currentImage.textMeta.Model.trim()
+                Dimension: `${this.#currentImage.textMeta.ImageSize.trim()} (${this.#currentImage.textMeta.Megapixels} Megapixels)`,
+                Aperture: this.#currentImage.textMeta.Aperture ? `f/${this.#currentImage.textMeta.Aperture}` : null,
+                "Shutter Speed": this.#currentImage.textMeta.ShutterSpeed?.trim(),
+                "Focal Length": this.#currentImage.textMeta.FocalLength?.trim(),
+                ISO: this.#currentImage.textMeta.ISO,
+                "White Balance": this.#currentImage.textMeta.WhiteBalance?.trim(),
+                "Camera Model": !!this.#currentImage.textMeta.Make
+                    ? !!this.#currentImage.textMeta.Model
+                        ? this.#currentImage.textMeta.Make.trim() + " " + this.#currentImage.textMeta.Model.trim()
+                        : this.#currentImage.textMeta.Make.trim()
+                    : !!this.#currentImage.textMeta.Model
+                        ? this.#currentImage.textMeta.Model.trim()
                         : null,
             },
             History: {
-                Date: this.currentImage.textMeta.CreateDate?.trim(),
-                Location: this.currentImage.textMeta.GPSPosition?.trim(),
+                Date: this.#currentImage.textMeta.CreateDate?.trim(),
+                Location: this.#currentImage.textMeta.GPSPosition?.trim(),
             },
         };
         for (const [sectionHeading, sectionData] of Object.entries(meta)) {
-            this.imageInfoTable.appendChild(SimpleGallery.tableFromObject(sectionData, sectionHeading));
+            this.#imageInfoTable.appendChild(SimpleGallery.tableFromObject(sectionData, sectionHeading));
         }
-        if (!!this.currentImage.gpsMeta.GPSLatitude && !!this.leafletLib) {
-            !!this.leafletMap && this.leafletMap.remove();
-            this.leafletMap = this.leafletLib
-                .map(this.imageLocationMap)
-                .setView([this.currentImage.gpsMeta.GPSLatitude, this.currentImage.gpsMeta.GPSLongitude], 14);
+        if (!!this.#currentImage.gpsMeta.GPSLatitude && !!this.leafletLib) {
+            !!this.#leafletMap && this.#leafletMap.remove();
+            this.#leafletMap = this.leafletLib
+                .map(this.#imageLocationMap)
+                .setView([this.#currentImage.gpsMeta.GPSLatitude, this.#currentImage.gpsMeta.GPSLongitude], 14);
             this.leafletLib
                 .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
                 maxZoom: 19,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             })
-                .addTo(this.leafletMap);
-            this.leafletLib.marker([this.currentImage.gpsMeta.GPSLatitude, this.currentImage.gpsMeta.GPSLongitude]).addTo(this.leafletMap);
-            this.imageLocationMap.classList.remove("hidden");
+                .addTo(this.#leafletMap);
+            this.leafletLib.marker([this.#currentImage.gpsMeta.GPSLatitude, this.#currentImage.gpsMeta.GPSLongitude]).addTo(this.#leafletMap);
+            this.#imageLocationMap.classList.remove("hidden");
         }
         else {
-            this.imageLocationMap.classList.add("hidden");
+            this.#imageLocationMap.classList.add("hidden");
         }
-        this.imageOverlay.classList.remove("gone");
+        this.#imageOverlay.classList.remove("gone");
     }
     toggleImageInfo() {
-        this.imageInfoPanel.classList.toggle("gone");
-        if (!!this.leafletMap) {
-            this.leafletMap.invalidateSize();
+        this.#imageInfoPanel.classList.toggle("gone");
+        if (!!this.#leafletMap) {
+            this.#leafletMap.invalidateSize();
         }
     }
     hideImage() {
-        this.imageOverlay.classList.add("gone");
+        this.#imageOverlay.classList.add("gone");
     }
     initialize() {
         this.leafletLib = window["L"];
-        this.albumListDisplay = document.querySelector("#album-list");
-        this.gallery = document.querySelector("#gallery-wrapper");
-        this.imageOverlay = document.querySelector("#image-wrapper");
-        this.imageOverlayCloseBtn = document.querySelector("#close-btn");
-        this.imageOverlayCloseBtn.addEventListener("click", () => {
+        this.#albumListDisplay = document.querySelector("#album-list");
+        this.#gallery = document.querySelector("#gallery-wrapper");
+        this.#imageOverlay = document.querySelector("#image-wrapper");
+        this.#imageOverlayCloseBtn = document.querySelector("#close-btn");
+        this.#imageOverlayCloseBtn.addEventListener("click", () => {
             this.hideImage();
         });
-        this.image = document.querySelector("#the-image");
-        this.image.onload = () => {
-            this.image.classList.remove("hidden");
+        this.#image = document.querySelector("#the-image");
+        this.#image.onload = () => {
+            this.#image.classList.remove("hidden");
         };
-        this.imageInfoPanel = document.querySelector("#image-info-panel");
-        this.imageInfoName = document.querySelector("#image-info-name");
-        this.imageInfoTable = document.querySelector("#image-info-table");
-        this.imageInfoButton = document.querySelector("#info-btn");
-        this.imageInfoButton.addEventListener("click", () => {
+        this.#imageInfoPanel = document.querySelector("#image-info-panel");
+        this.#imageInfoName = document.querySelector("#image-info-name");
+        this.#imageInfoTable = document.querySelector("#image-info-table");
+        this.#imageInfoButton = document.querySelector("#info-btn");
+        this.#imageInfoButton.addEventListener("click", () => {
             this.toggleImageInfo();
         });
-        this.imageNextButton = document.querySelector("#next-btn");
-        this.imageNextButton.addEventListener("click", () => {
-            let index = this.galleryImages.findIndex((img) => img.id === this.currentImage.id);
-            this.displayImage(this.galleryImages[index + 1]?.id);
+        this.#imageNextButton = document.querySelector("#next-btn");
+        this.#imageNextButton.addEventListener("click", () => {
+            let index = this.#galleryImages.findIndex((img) => img.id === this.#currentImage.id);
+            this.displayImage(this.#galleryImages[index + 1]?.id);
         });
-        this.imagePreviousButton = document.querySelector("#previous-btn");
-        this.imagePreviousButton.addEventListener("click", () => {
-            let index = this.galleryImages.findIndex((img) => img.id === this.currentImage.id);
-            this.displayImage(this.galleryImages[index - 1]?.id);
+        this.#imagePreviousButton = document.querySelector("#previous-btn");
+        this.#imagePreviousButton.addEventListener("click", () => {
+            let index = this.#galleryImages.findIndex((img) => img.id === this.#currentImage.id);
+            this.displayImage(this.#galleryImages[index - 1]?.id);
         });
-        this.imageLocationMap = document.querySelector("#location-map");
-        this.dbHelper = new GalleryDBHelper("gallery_index", 1);
+        this.#imageLocationMap = document.querySelector("#location-map");
+        this.#dbHelper = new GalleryDBHelper("gallery_index", 1);
         this.loadImages(() => {
             document.querySelector("#loading-dialog")?.remove();
             this.displayAlbums();
@@ -201,15 +208,15 @@ class SimpleGallery {
     }
 }
 class DBHelper {
+    #dbName;
+    #dbVersion;
+    #dbInstance;
     constructor(name, version) {
-        _DBHelper_dbName.set(this, void 0);
-        _DBHelper_dbVersion.set(this, void 0);
-        _DBHelper_dbInstance.set(this, void 0);
-        __classPrivateFieldSet(this, _DBHelper_dbName, name, "f");
+        this.#dbName = name;
         if (!!!version) {
             throw new Error("Database vserion is not valid!");
         }
-        __classPrivateFieldSet(this, _DBHelper_dbVersion, version, "f");
+        this.#dbVersion = version;
     }
     onUpgrade(db, oldVersion, newVersion) { }
     onDelete(db) { }
@@ -371,7 +378,7 @@ class DBHelper {
             request.onsuccess = (event) => {
                 const db = event.target.result;
                 this.onOpen(db);
-                __classPrivateFieldSet(this, _DBHelper_dbInstance, db, "f");
+                this.#dbInstance = db;
                 resolve(db);
             };
             request.onerror = (event) => {
@@ -389,16 +396,15 @@ class DBHelper {
         }
     }
     get activeDatabaseName() {
-        return __classPrivateFieldGet(this, _DBHelper_dbName, "f");
+        return this.#dbName;
     }
     get activeDatabase() {
-        return __classPrivateFieldGet(this, _DBHelper_dbInstance, "f");
+        return this.#dbInstance;
     }
     get activeDatabaseVersion() {
-        return __classPrivateFieldGet(this, _DBHelper_dbVersion, "f");
+        return this.#dbVersion;
     }
 }
-_DBHelper_dbName = new WeakMap(), _DBHelper_dbVersion = new WeakMap(), _DBHelper_dbInstance = new WeakMap();
 class GalleryDBHelper extends DBHelper {
     onCreate(db) {
         const objectStore = db.createObjectStore("images", { keyPath: "id" });
